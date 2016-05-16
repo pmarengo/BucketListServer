c2461083fa3c4f6451cdbe3a62bf3abb7b181886
pmarengo <pmarengo@e-mozart.com>
2016-05-16 14:51:22 -0300

    change URI services.js

--- dev/client/bucketListApp/www/js/services.js
+++ dev/client/bucketListApp/www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('bucketList.services', [])
     .factory('API', function ($rootScope, $http, $ionicLoading, $window) {
-       var base = "http://bucketlistapplication.herokuapp.com";
+       var base = "http://localhost:9804/api/v1/bucketList";
         $rootScope.show = function (text) {
             $rootScope.loading = $ionicLoading.show({
                 content: text ? text : 'Loading',
